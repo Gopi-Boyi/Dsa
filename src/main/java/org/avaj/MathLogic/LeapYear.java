d3fd46d9207dfccbbdfda6c4809b9ad1fcbfc9d6
Gopi Boyi <gopiboyi88@gmail.com>
2026-07-28 18:49:20 +0530

    problem solving

--- src/main/java/org/avaj/MathLogic/LeapYear.java
+++ src/main/java/org/avaj/MathLogic/LeapYear.java
@@ -5,11 +5,18 @@ public class LeapYear
     public static String leap(int n){
         if(n%400 == 0){
             return "It's a Leap Year";
-        } else if (n%400 ==0 && n%100 !=0) {
+        } else if (n%4 ==0 && n%100 !=0) {
             return "It's a Leap Year";
         }else
             return "It's not a leap year";
 
+    }
+    public static void main (String[] args){
+        int n = 2008;
+
+        String result=leap(n);
+
+        System.out.print(result);
     }
 
 }
