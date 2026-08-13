a4d00b20add9ae4c5cfa2d0ccf8bdf232a707ed8
Gopi Boyi <gopiboyi88@gmail.com>
2026-08-13 20:21:54 +0530

    Something fixed

--- src/main/java/org/avaj/MathLogic/ReverseElements.java
+++ src/main/java/org/avaj/MathLogic/ReverseElements.java
@@ -1,14 +1,24 @@
 package org.avaj.MathLogic;
 
+import jdk.jshell.execution.JdiDefaultExecutionControl;
+
 public class ReverseElements {
     public static void main(String[] args){
-        int n = 1234;
+        int n = 2147483644;
         int rev = 0;
         while(n > 0){
             int digit = n % 10;
-            rev = rev * 10 + digit;
+
             n = n / 10;
+
+            if(rev > Integer.MAX_VALUE / 10 || rev == Integer.MAX_VALUE / 10 && digit > 7){
+                System.out.print("0");
+            }
+            if(rev < Integer.MIN_VALUE / 10 || rev == Integer.MIN_VALUE /10 && digit < -8){
+                System.out.print("0");
+            }
+            rev = rev * 10 + digit;
         }
-        System.out.print(rev);
+    System.out.print(rev);
     }
 }
