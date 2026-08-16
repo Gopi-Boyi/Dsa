3ff07aad3f7a39dc402d2381297c66c3cfec2ff8
Gopi Boyi <gopiboyi88@gmail.com>
2026-08-16 22:59:53 +0530

    resloved some error

--- src/main/java/org/avaj/LargestElement.java
+++ src/main/java/org/avaj/LargestElement.java
@@ -3,7 +3,7 @@ package org.avaj;
 public class LargestElement {
     public static void main(String[] args){
 
-        int[] arr={12,34,56,78,98,56,122};
+        int[] arr={12,34,56,78,};
 
         int max=arr[0];
         for(int i=1; i< arr.length; i++){
