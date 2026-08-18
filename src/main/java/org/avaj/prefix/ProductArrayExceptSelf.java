ddd10b48ebfc571a885e404e6191658d3cc6539c
Gopi Boyi <gopiboyi88@gmail.com>
2026-08-18 22:39:49 +0530

    added pattern name

--- src/main/java/org/avaj/prefix/ProductArrayExceptSelf.java
+++ src/main/java/org/avaj/prefix/ProductArrayExceptSelf.java
@@ -20,7 +20,7 @@ public class ProductArrayExceptSelf {
         for (int i = 1; i < n; i++) {
             answer[i] = answer[i - 1] * nums[i - 1];
         }
-        int suffix = 1;
+        int suffix = 1;                                 //PREFIX PATTERN
 
         for (int i = n - 1; i >= 0; i--) {
             answer[i] = answer[i] * suffix;
